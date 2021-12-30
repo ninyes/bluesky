ca58de5c1afa45114cff00053527864b056f26b4
Nino <ninonyessen@gmail.com>
2021-12-30 15:38:22 +0100

    Added state logger to assess state change patterns before LoS

--- plugins/performancelogger.py
+++ plugins/performancelogger.py
@@ -41,8 +41,8 @@ fuelheader = \
 # Log parameters for the los of separations log
 losheader = \
     '#######################################################\n' + \
-    'CONF LOG\n' + \
-    'Conflict Statistics\n' + \
+    'LOS LOG\n' + \
+    'Los of Separation Statistics\n' + \
     '#######################################################\n\n' + \
     'Parameters [Units]:\n' + \
     'Simulation time [s], ' + \
@@ -63,7 +63,7 @@ losheader = \
 # Log parameters for the traffic density log
 densheader = \
     '#######################################################\n' + \
-    'CONF LOG\n' + \
+    'DENS LOG\n' + \
     'Density Statistics\n' + \
     '#######################################################\n\n' + \
     'Parameters [Units]:\n' + \
@@ -76,6 +76,29 @@ densheader = \
     'Average true airspeed (G/S) [m/s], ' + \
     'Average nominal path distance (G/S) [m], ' + \
     'Traffic density (G/S) [AC/1000km^2] \n'
+
+# Log parameters for periodic logger of aircraft states
+stateheader = \
+    '#######################################################\n' + \
+    'STATE LOG\n' + \
+    'Periodic State Logger\n' + \
+    '#######################################################\n\n' + \
+    'Parameters [Units]:\n' + \
+    'Simulation time [s], ' + \
+    'ACID [-], ' + \
+    'Latitude [deg], ' + \
+    'Longitude [deg], ' + \
+    'Heading [deg], ' + \
+    'Track [deg], ' + \
+    'AP Track [deg], ' + \
+    'Altitude [m], ' + \
+    'Sel alt [m], ' + \
+    'Vertical Speed [m/s], ' + \
+    'Sel vs [m/s], ' + \
+    'Mach [-], ' + \
+    'Ground speed [m/s], ' + \
+    'CAS [m/s], ' + \
+    'Sel spd [m/s or -] \n'
 
 # Global data
 perflog = None
@@ -146,14 +169,16 @@ class PerformanceLogger(Entity):
         self.p_GS   = path.Path(np.array(coords[0]))
 
         # The loggers
-        self.fuellog = datalog.crelog('FUELLOG', None, fuelheader)
-        self.loslog  = datalog.crelog('LOSLOG', None, losheader)
-        self.denslog = datalog.crelog('DENSLOG', None, densheader)
+        self.fuellog  = datalog.crelog('FUELLOG', None, fuelheader)
+        self.loslog   = datalog.crelog('LOSLOG', None, losheader)
+        self.denslog  = datalog.crelog('DENSLOG', None, densheader)
+        self.statelog = datalog.crelog('STATELOG', None, stateheader)
 
         # Start the loggers
         self.fuellog.start()
         self.loslog.start()
         self.denslog.start()
+        self.statelog.start()
 
     def reset(self):
         ''' Reset area state when simulation is reset. '''
@@ -255,4 +280,9 @@ class PerformanceLogger(Entity):
             self.denslog.log(np.sum(inreg), avg_tot_spd, avg_tot_dist, ac_dens,
                              np.sum(inreg_GS), avg_tot_spd_GS, avg_tot_dist_GS, ac_dens_GS)
             
-            
+        # Log aircraft states every 10 seconds
+        if sim.simt % 10 == 0:
+            self.statelog.log(traf.id, traf.lat, traf.lon, traf.hdg, traf.trk, 
+                              (traf.ap.trk + 360)%360, traf.alt, traf.selalt, 
+                              traf.vs, traf.selvs, traf.M, traf.gs, traf.cas,
+                              traf.selspd)
